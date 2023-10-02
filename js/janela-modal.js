6988a7934a2da786c3e1d825292e34a86e61ace7
CarolPrado08 <carolprado0820@gmail.com>
2023-10-01 22:23:44 -0300

    Página de projetos atualizada

---
+++ js/janela-modal.js
@@ -0,0 +1,13 @@
+function abrir(){
+
+    let modal = document.querySelector('.janela-modal')
+
+    modal.style.display = 'block';
+}
+
+function fechar(){
+    
+    let modal = document.querySelector('.janela-modal')
+
+    modal.style.display = 'none'
+}
